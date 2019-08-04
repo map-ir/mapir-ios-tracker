1811d612a17d5c673377b2db6efe9c5c87811f7b
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-08-04 14:22:37 +0430

    Cartfile and NetworkUtilities added.

---
+++ Sources/NetworkUtilities.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkUtilities.swift
+//  MapirLiveTracker-iOS
+//
+//  Created by Alireza Asadi on 13/5/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
