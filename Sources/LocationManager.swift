3bbb79555e7250c856ae6b42285d8781075b55cc
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-08-06 15:59:13 +0430

    added location manager functionality to the class

--- Sources/LocationManager.swift
+++ Sources/LocationManager.swift
@@ -10,5 +10,50 @@ import Foundation
 import CoreLocation
 
 final class LocationManager {
-    
+    let locationManager = CLLocationManager()
+
+    enum Status {
+        case initiated
+        case tracking
+        case stopped
+    }
+
+    var status: LocationManager.Status = .initiated
+
+    init(distanceFilter: Double) {
+        if distanceFilter <= 10 {
+            locationManager.distanceFilter = 10
+        }
+        else {
+            locationManager.distanceFilter = distanceFilter
+        }
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.pausesLocationUpdatesAutomatically = false
+    }
+
+    func startTracking() throws {
+        try authorizationCheck()
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+        status = .tracking
+    }
+
+    func stopTracking() {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
+        status = .stopped
+    }
+
+    func authorizationCheck() throws {
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined, .authorizedWhenInUse, .denied, .restricted:
+            throw TrackingError.LocationServiceError.unauthorizedForAlwaysUsage
+        case .authorizedAlways:
+            locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
+        @unknown default:
+            break
+        }
+    }
 }
