3be670045944422e89353d437024b45ecb108470
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-08-06 10:16:35 +0430

    LocationManager Added.

---
+++ Sources/LocationManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocationManager.swift
+//  MapirLiveTracker-iOS
+//
+//  Created by Alireza Asadi on 15/5/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+final class LocationManager {
+    
+}
