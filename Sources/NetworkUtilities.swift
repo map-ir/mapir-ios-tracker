c450b2b4da85606c465d4afa9200cf05b698697d
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-08-09 11:51:40 +0430

    added user agent property.

--- Sources/NetworkUtilities.swift
+++ Sources/NetworkUtilities.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+struct NetworkUtilities {
+    static let shared = NetworkUtilities()
+    private init() { }
+
+    let userAgent: String = {
+        var components: [String] = []
+
+        if let appName = Bundle.main.infoDictionary?["CFBundleName"] as? String ?? Bundle.main.infoDictionary?["CFBundleIdentifier"] as?String {
+            let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+            components.append("\(appName)/\(version)")
+        }
+
+        // Replace Your bundle name
+        let libraryBundle: Bundle? = Bundle(for: MapirLiveTrackerPublisher.self)
+
+        if let libraryName = libraryBundle?.infoDictionary?["CFBundleName"] as? String, let version = libraryBundle?.infoDictionary?["CFBundleShortVersionString"] as? String {
+            components.append("\(libraryName)/\(version)")
+        }
+
+        let system: String
+        #if os(OSX)
+            system = "macOS"
+        #elseif os(iOS)
+            system = "iOS"
+        #elseif os(watchOS)
+            system = "watchOS"
+        #elseif os(tvOS)
+            system = "tvOS"
+        #endif
+        let systemVersion = ProcessInfo().operatingSystemVersion
+        components.append("\(system)/\(systemVersion.majorVersion).\(systemVersion.minorVersion).\(systemVersion.patchVersion)")
+
+        let chip: String
+        #if arch(x86_64)
+            chip = "x86_64"
+        #elseif arch(arm)
+            chip = "arm"
+        #elseif arch(arm64)
+            chip = "arm64"
+        #elseif arch(i386)
+            chip = "i386"
+        #endif
+        components.append("(\(chip))")
+
+        return components.joined(separator: " ")
+    }()
+
+    let defaultURLComponents: URLComponents = {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "dev.map.ir"
+        urlComponents.path = "/tracking"
+        return urlComponents
+    }()
+}
