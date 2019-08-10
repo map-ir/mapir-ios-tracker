75c89135f426ad8878ccb12086f34aa25e84afe5
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-08-10 14:54:08 +0430

    Added storyboard to the example project.

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,35 +9,9 @@
 import UIKit
 import MapirLiveTracker
 
-// MARK: - ViewController
-
-/// The ViewController
-class ViewController: UIViewController {
-
-    // MARK: Properties
-    
-    /// The Label
-    lazy var label: UILabel = {
-        let label = UILabel()
-        label.text = "🚀\nMapirLiveTracker\nExample"
-        label.font = .systemFont(ofSize: 25, weight: .semibold)
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
-        label.textAlignment = .center
-        return label
-    }()
-    
-    // MARK: View-Lifecycle
-    
+class MainViewController: UIViewController {
     /// View did load
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
     }
-    
-    /// LoadView
-    override func loadView() {
-        self.view = self.label
-    }
-
 }
