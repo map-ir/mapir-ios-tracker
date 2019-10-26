849a204256ee5ce5f59b7cbb1725e398df1cae7a
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-10-26 13:20:19 +0330

    Added sample for Objective-C. Updated scheme names.

---
+++ Objective-C Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective-C Example
+//
+//  Created by Alireza Asadi on 4/8/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Objective-C Example/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Objective-C Example
+//
+//  Created by Alireza Asadi on 4/8/1398 AP.
+//  Copyright © 1398 AP Map. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
